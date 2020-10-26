5f4a3df34330d0b2e8249a4fbab8f9cf7286e57d
junyi <collau@hotmail.com>
2020-10-26 09:39:04 +0800

    Intents: Using Bundle to retrieve data and Integers

--- app/src/main/java/com/fishnco/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/fishnco/activitylifecycle/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button showGuess;
+    private EditText enterGuess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +20,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         showGuess = findViewById(R.id.button_guess);
+        enterGuess = findViewById(R.id.textFieldName);
         showGuess.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ShowGuess.class);
-                intent.putExtra("guess", "Hello there");
-                startActivity(intent);
+                String guess = enterGuess.getText().toString().trim();
+
+                if (!guess.isEmpty()) {
+                    Intent intent = new Intent(MainActivity.this, ShowGuess.class);
+                    intent.putExtra("guess", guess);
+                    intent.putExtra("age", 34);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Enter Guess", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+
     }
 }
--- app/src/main/java/com/fishnco/activitylifecycle/ShowGuess.java
+++ app/src/main/java/com/fishnco/activitylifecycle/ShowGuess.java
@@ -3,6 +3,7 @@ package com.fishnco.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class ShowGuess extends AppCompatActivity {
@@ -14,12 +15,19 @@ public class ShowGuess extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_guess);
 
+        Bundle extra = getIntent().getExtras();
+
         textView_receivedText = findViewById(R.id.textView_receivedText);
 
-        if (getIntent().getStringExtra("guess") != null) {
-            String value = getIntent().getStringExtra("guess");
-            textView_receivedText.setText(value);
+        if (extra != null) {
+            textView_receivedText.setText(extra.getString("guess"));
+            Log.d("ShowGuess", Integer.toString(extra.getInt("age")));
         }
+
+//        if (getIntent().getStringExtra("guess") != null) {
+//            String value = getIntent().getStringExtra("guess");
+//            textView_receivedText.setText(value);
+//        }
 
     }
 }
